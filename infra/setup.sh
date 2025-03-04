f8a073ac42ba9887757a319b267b8365d333f1df
jhs512 <jangka512@gmail.com>
2025-03-04 09:48:41 +0900

    work

--- infra/setup.sh
+++ infra/setup.sh
@@ -1,8 +1,11 @@
+# 기존 컨테이너 삭제
 docker rm -f mysql-1 mysql-2 mysql-3 2>/dev/null
 rm -rf dockerProjects/mysql-1 dockerProjects/mysql-2 dockerProjects/mysql-3
 
+# common 네트워크 생성
 docker network create common 2>/dev/null
 
+# mysql-1(소스서버) 설정파일 생성
 mkdir -p dockerProjects/mysql-1/volumes/etc/mysql/conf.d
 chmod 644 dockerProjects/mysql-1/volumes/etc/mysql/conf.d/my.cnf 2>/dev/null
 cat <<EOF > dockerProjects/mysql-1/volumes/etc/mysql/conf.d/my.cnf
@@ -16,6 +19,7 @@ log-slave-updates=ON
 EOF
 chmod 444 dockerProjects/mysql-1/volumes/etc/mysql/conf.d/my.cnf
 
+# mysql-2(리플리카1 서버) 설정파일 생성
 mkdir -p dockerProjects/mysql-2/volumes/etc/mysql/conf.d
 chmod 644 dockerProjects/mysql-2/volumes/etc/mysql/conf.d/my.cnf 2>/dev/null
 cat <<EOF > dockerProjects/mysql-2/volumes/etc/mysql/conf.d/my.cnf
@@ -31,6 +35,7 @@ relay_log_recovery=ON
 EOF
 chmod 444 dockerProjects/mysql-2/volumes/etc/mysql/conf.d/my.cnf
 
+# mysql-3(리플리카2 서버) 설정파일 생성
 mkdir -p dockerProjects/mysql-3/volumes/etc/mysql/conf.d
 chmod 644 dockerProjects/mysql-3/volumes/etc/mysql/conf.d/my.cnf 2>/dev/null
 cat <<EOF > dockerProjects/mysql-3/volumes/etc/mysql/conf.d/my.cnf
@@ -46,6 +51,7 @@ relay_log_recovery=ON
 EOF
 chmod 444 dockerProjects/mysql-3/volumes/etc/mysql/conf.d/my.cnf
 
+# mysql-1 서버 실행
 docker run -d --name mysql-1 --network common \
   --restart unless-stopped \
   -v /${PWD}/dockerProjects/mysql-1/volumes/var/lib/mysql:/var/lib/mysql \
@@ -55,8 +61,13 @@ docker run -d --name mysql-1 --network common \
   -e TZ=Asia/Seoul \
   mysql:latest
 
+# 원활한 설정을 위해서 잠시 대기
 sleep 20
 
+# mysql-1 서버 초기화
+## 리플리케이션 동기화용 계정 repl 생성 및 권한 부여
+## 사용계정 lldj 생성 및 권한 부여
+## 데이터베이스 surl 생성
 docker exec mysql-1 mysql -uroot -plldj123414 -e "
 CREATE USER 'repl'@'%' IDENTIFIED WITH caching_sha2_password BY 'replpass';
 GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
@@ -69,6 +80,7 @@ CREATE DATABASE surl;
 FLUSH PRIVILEGES;
 "
 
+# mysql-2 서버 실행
 docker run -d --name mysql-2 --network common \
   --restart unless-stopped \
   -v /${PWD}/dockerProjects/mysql-2/volumes/var/lib/mysql:/var/lib/mysql \
@@ -78,8 +90,10 @@ docker run -d --name mysql-2 --network common \
   -e TZ=Asia/Seoul \
   mysql:latest
 
+# 원활한 설정을 위해서 잠시 대기
 sleep 20
 
+# mysql-2 서버 초기화, 리플리케이션(동기화) 시작
 docker exec mysql-2 mysql -uroot -plldj123414 -e "
 CHANGE REPLICATION SOURCE TO
   SOURCE_HOST='mysql-1',
@@ -90,10 +104,12 @@ CHANGE REPLICATION SOURCE TO
 START REPLICA;
 "
 
+# mysql-2 리플리케이션 상태 확인
 docker exec mysql-2 mysql -uroot -plldj123414 -e "
 SHOW REPLICA STATUS\G;
 "
 
+# mysql-3 서버 실행
 docker run -d --name mysql-3 --network common \
   --restart unless-stopped \
   -v /${PWD}/dockerProjects/mysql-3/volumes/var/lib/mysql:/var/lib/mysql \
@@ -103,8 +119,10 @@ docker run -d --name mysql-3 --network common \
   -e TZ=Asia/Seoul \
   mysql:latest
 
+# 원활한 설정을 위해서 잠시 대기
 sleep 20
 
+# mysql-3 서버 초기화, 리플리케이션(동기화) 시작
 docker exec mysql-3 mysql -uroot -plldj123414 -e "
 CHANGE REPLICATION SOURCE TO
   SOURCE_HOST='mysql-1',
@@ -115,6 +133,7 @@ CHANGE REPLICATION SOURCE TO
 START REPLICA;
 "
 
+# mysql-3 리플리케이션 상태 확인
 docker exec mysql-3 mysql -uroot -plldj123414 -e "
 SHOW REPLICA STATUS\G;
 "
